185664db8c31a6cd1e96094ac8a3f7707e1ca569
NtshetsoPele <mothobi.bald@gmail.com>
2024-07-08 21:45:49 +0200

    first commit

---
+++ 1.GettingStarted/Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,9 @@
+﻿using MovieList = System.Collections.Generic.IEnumerable<Movies.Contracts.Responses.MoviesResponse>;
+
+namespace Movies.Contracts.Responses;
+
+public class MoviesResponse
+{
+    //public required IEnumerable<MoviesResponse> Items { get; init; } = Enumerable.Empty<MoviesResponse>();
+    public required MovieList Items { get; init; } = Enumerable.Empty<MoviesResponse>();
+}
